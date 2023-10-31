e39cb4f17f249f309b6efdcefe90fd72ec4335cd
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-31 21:27:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/final-project-frontend">
+      <BrowserRouter basename="/react-node-fs78-OleksiiVTS">
         <ScrollBarStyles />
         <App />
       </BrowserRouter>
